4f3aded4c8b0ea3242bee22aa058dddff7826002
Professional <nasibaukr@mail.ru>
2023-06-06 17:23:59 +0600

    all sms

--- semple/bboard/urls.py
+++ semple/bboard/urls.py
@@ -3,8 +3,8 @@ from .views import SMSListView, create_sms, delete_sms
 
 urlpatterns = [
     path('create/', create_sms, name='create_sms'),
-    path('sms/delete/<int:sms_id>/', delete_sms, name='delete_sms'),
     path('', SMSListView.as_view(), name='sms_list'),
+    path('sms/delete/<int:sms_id>/', delete_sms, name='delete_sms'),
 
 ]
 
