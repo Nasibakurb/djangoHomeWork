acc5b6da794f1288f272b98882668c651113719d
Professional <nasibaukr@mail.ru>
2023-06-11 11:23:51 +0600

    all sms

--- semple/bboard/urls.py
+++ semple/bboard/urls.py
@@ -3,19 +3,19 @@ from .views import SMSListView, create_sms, delete_sms, read_sms, get_sms
 
 urlpatterns = [
     path('', SMSListView.as_view(), name='sms_list'),
-    # re_path(r'^$', SMSListView.as_view(), name='sms_list'),
+    # re_path(r^$', SMSListView.as_view(), name='sms_list'),
 
     path('create/', create_sms, name='create_sms'),
-    # path('create/$', create_sms, name='create_sms'),
+    # re_path(r'create/$', create_sms, name='create_sms'),
 
     path('delete/<int:sms_id>/', delete_sms, name='delete_sms'),
-    # path('delete/(?P<sms_id>\d+)/$', delete_sms, name='delete_sms'),
+    # re_path(r'delete/(?P<sms_id>\d+)/$', delete_sms, name='delete_sms'),
 
-    path('sms/<int:sms_id>', read_sms, name='sms_read'),
-    # path('sms/(?P<sms_id>\d+)/$', read_sms, name='sms_read'),
+    path('sms/<int:sms_id>/', read_sms, name='sms_read'),
+    # re_path(r'sms/(?P<sms_id>\d+)/$', read_sms, name='sms_read'),
 
-    path('get/', get_sms, name='get_sms')
-    # path('get/$', get_sms, name='get_sms')
+    path('get/<int:sms_id>/', get_sms, name='get_sms')
+    # re_path(r'get/$', get_sms, name='get_sms')
 ]
 
 
